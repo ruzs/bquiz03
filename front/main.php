25a9cd08cee7571d6fbe3d44871a750a6200f636
ruzs <eddie10913@gmail.com>
2023-02-03 16:20:56 +0800

    建立前台院線片資料撈取語法及呈現資料內容

--- front/main.php
+++ front/main.php
@@ -1,22 +1,43 @@
 <div class="half" style="vertical-align:top;">
-      <h1>預告片介紹</h1>
-      <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-          <ul class="lists">
-          </ul>
-          <ul class="controls">
-          </ul>
+  <h1>預告片介紹</h1>
+  <div class="rb tab" style="width:95%;">
+    <div id="abgne-block-20111227">
+      <ul class="lists">
+      </ul>
+      <ul class="controls">
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="half">
+  <h1>院線片清單</h1>
+  <div class="rb tab" style="width:95%;">
+    <div style="display:flex;flex-wrap:wrap">
+      <?php
+      $today = date("Y-m-d");
+      $ondate = date("Y-m-d", strtotime("-2 days"));
+      ///$rows=$Movie->all(['sh'=>1]," && `ondate` >= '$ondate' && `ondate` <= '$today' order by `rank`");
+      $rows = $Movie->all(['sh' => 1], " && `ondate` between '$ondate' AND '$today' order by `rank`");
+
+      foreach ($rows as $row) {
+      ?>
+        <div>
+          <div>片名:<?= $row['name']; ?></div>
+          <div></div>
+          <div>
+            <button onclick="location.href='?do=intro&id=<?= $row['id']; ?>'">劇情簡介</button>
+            <button onclick="location.href='?do=order&id=<?= $row['id']; ?>'">線上訂票</button>
+          </div>
+
+
         </div>
-      </div>
+
+      <?php
+
+      }
+      ?>
+
     </div>
-    <div class="half">
-      <h1>院線片清單</h1>
-      <div class="rb tab" style="width:95%;">
-        <table>
-          <tbody>
-            <tr> </tr>
-          </tbody>
-        </table>
-        <div class="ct"> </div>
-      </div>
-    </div>
+    <div class="ct"> </div>
+  </div>
+</div>
