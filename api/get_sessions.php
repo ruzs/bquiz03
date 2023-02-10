ebd191b8df59d70ad6341d0d7ce67fc76a86b9ae
ruzs <eddie10913@gmail.com>
2023-02-10 13:38:22 +0800

    建立訂票選單及劃位畫面間的切換機制,使用js

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -8,8 +8,13 @@ if ($date === date("Y-m-d") && $hr >= 14) {
   $start = 1;
 }
 for ($i = $start; $i <= 5; $i++) {
+   /**
+     * 1.先找出該場次的所有訂位紀錄(電影,日期,場次)  $Order->all(['movie'=>$movie,'date'=>$date,'session'=>$session]);
+     * 2.算出所有訂位紀錄的座位總數  foreach($orders as $order){ $seats+=$order['seats']}
+     * 3.計算20-總數=剩餘座位  20-$seats
+     */
   echo "<option value='{$Movie->session[$i]}'>";
   echo $Movie->session[$i];
-  echo " 剩餘座位 20 ";
+  echo " 剩餘座位 20 ";//20-已被訂走的座位數  
   echo "</option>";
 }
--- front/order.php
+++ front/order.php
@@ -1,67 +1,71 @@
-<h3 class="ct">線上訂票</h3>
-
-<table style="width:50%;margin:auto">
-  <tr>
-    <td>電影:</td>
-    <td>
-      <select name="" id="movie"></select>
-    </td>
-  </tr>
-  <tr>
-    <td>日期:</td>
-    <td>
-      <select name="" id="day"></select>
-    </td>
-  </tr>
-  <tr>
-    <td>場次:</td>
-    <td>
-      <select name="" id="session"></select>
-    </td>
-  </tr>
-</table>
-<div class="ct">
-  <button>確定</button>
-  <button>重置</button>
+<div id="orderForm">
+  <h3 class="ct">線上訂票</h3>
+  <table style="width:50%;margin:auto">
+    <tr>
+      <td>電影:</td>
+      <td>
+        <select name="" id="movie"></select>
+      </td>
+    </tr>
+    <tr>
+      <td>日期:</td>
+      <td>
+        <select name="" id="day"></select>
+      </td>
+    </tr>
+    <tr>
+      <td>場次:</td>
+      <td>
+        <select name="" id="session"></select>
+      </td>
+    </tr>
+  </table>
+  <div class="ct">
+    <button onclick="$('#orderForm,#booking').toggle();getBooking()">確定</button>
+    <button>重置</button>
+  </div>
 </div>
-<script>
-  getMovies();
-  $("#movie").on("change", function() {
-    getDays($("#movie").val());
-  })
-  $("#day").on("change", function() {
-    getSessions($("#movie").val(), $("#day").val());
-  })
-
-  function getMovies() {
-    let params = {}
-    location.href.split("?")[1].split("&").forEach(item => {
-      params[item.split("=")[0]] = item.split("=")[1]
-    })
-    $.get("./api/get_movies.php", (movies) => {
-      $("#movie").html(movies)
-      if (params.id) {
-        $(`#movie option[value="${params.id}"]`).attr("selected", true)
-      }
+<div id="booking" style="display:none">
+  <div class="ct">
+    <button onclick="$('#orderForm,#booking').toggle();$('#booking').html('')">上一步</button>
+    <button>確定</button>
+  </div>
+</div>
+  <script>
+    getMovies();
+    $("#movie").on("change", function() {
       getDays($("#movie").val());
     })
-  }
-
-  function getDays(id) {
-    $.get("./api/get_days.php", {
-      id
-    }, (days) => {
-      $("#day").html(days);
-      getSessions(id, $("#day").val())
+    $("#day").on("change", function() {
+      getSessions($("#movie").val(), $("#day").val());
     })
-  }
-
-  function getSessions(id, date) {
-    $.get("./api/get_sessions.php", {
-      id,
-      date
-    }, (sessions) => {
-      $("#session").html(sessions);
-    })
-  }
-</script>
+    function getMovies() {
+      let params = {}
+      location.href.split("?")[1].split("&").forEach(item => {
+        params[item.split("=")[0]] = item.split("=")[1]
+      })
+      $.get("./api/get_movies.php", (movies) => {
+        $("#movie").html(movies)
+        if (params.id) {
+          $(`#movie option[value="${params.id}"]`).attr("selected", true)
+        }
+        getDays($("#movie").val());
+      })
+    }
+    function getDays(id) {
+      $.get("./api/get_days.php", {
+        id
+      }, (days) => {
+        $("#day").html(days);
+        getSessions(id, $("#day").val())
+      })
+    }
+    function getSessions(id, date) {
+      $.get("./api/get_sessions.php", {
+        id,
+        date
+      }, (sessions) => {
+        $("#session").html(sessions);
+      })
+    }
+  </script>
