c3b36e8502f40cc1c233c60f83cc6c0dfce349b5
ruzs <eddie10913@gmail.com>
2023-02-10 12:07:40 +0800

    建立取得電影上映日期的聯動函式getDays(id)

---
+++ api/get_days.php
@@ -0,0 +1,13 @@
+<?php
+include_once "base.php";
+$movie=$Movie->find($_GET['id']);
+$ondate=$movie['ondate'];
+$today=strtotime(date("Y-m-d"));
+$duration=3 - (( $today - strtotime($ondate))/(60*60*24));
+//echo $duration;
+for($i=0;$i<$duration;$i++){    
+    $date=date("Y-m-d",strtotime("+$i days"));
+    echo "<option value='$date'>$date</option>";
+}
+
+?>
--- front/order.php
+++ front/order.php
@@ -1,42 +1,51 @@
 <h3 class="ct">線上訂票</h3>
 
 <table style="width:50%;margin:auto">
-    <tr>
-        <td>電影:</td>
-        <td>
-            <select name="" id="movie"></select>
-        </td>
-    </tr>
-    <tr>
-        <td>日期:</td>
-        <td>
-            <select name="" id=""></select>
-        </td>
-    </tr>
-    <tr>
-        <td>場次:</td>
-        <td>
-            <select name="" id=""></select>
-        </td>
-    </tr>
+  <tr>
+    <td>電影:</td>
+    <td>
+      <select name="" id="movie"></select>
+    </td>
+  </tr>
+  <tr>
+    <td>日期:</td>
+    <td>
+      <select name="" id="day"></select>
+    </td>
+  </tr>
+  <tr>
+    <td>場次:</td>
+    <td>
+      <select name="" id=""></select>
+    </td>
+  </tr>
 </table>
 <div class="ct">
-<button>確定</button>
-<button>重置</button>
+  <button>確定</button>
+  <button>重置</button>
 </div>
 <script>
-getMovies();
-getMovies();
-function getMovies(){
-    let params={}
-    location.href.split("?")[1].split("&").forEach(item => { params[item.split("=")[0]]=item.split("=")[1] })
-    $.get("./api/get_movies.php",(movies)=>{
-        $("#movie").html(movies)
-        if(params.id){
-            $(`#movie option[value="${params.id}"]`).attr("selected",true)
-        }
+  getMovies();
+  $("#movie").on("change",function(){
+    getDays($("#movie").val());
+})
+
+  function getMovies() {
+    let params = {}
+    location.href.split("?")[1].split("&").forEach(item => {
+      params[item.split("=")[0]] = item.split("=")[1]
     })
-}
-function getDays(){
-}
+    $.get("./api/get_movies.php", (movies) => {
+      $("#movie").html(movies)
+      if (params.id) {
+        $(`#movie option[value="${params.id}"]`).attr("selected", true)
+      }
+      getDays($("#movie").val());
+    })
+  }
+
+  function getDays(id){
+    $.get("./api/get_days.php",{id},(days)=>{
+        $("#day").html(days);
+    })}
 </script>
